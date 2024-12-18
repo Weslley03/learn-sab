b27a771feb38666322dab276ec80e05edb8a79d3
oweslley <wesleymoraeswv03@hotmail.com>
2024-12-18 17:30:50 -0300

    project init

---
+++ main.js
@@ -0,0 +1,5 @@
+const imageInput = document.querySelector('#image-input');
+const canvas = document.querySelector('#canvas');
+const processButton = document.querySelector('#process-button');
+const ctx = canvas.getContext('2d');
+
